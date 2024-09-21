6378e9153833b80e0aca63bb6e4d79f7137a3ba9
GANJI SUBHASHINI <subhashinilucky05@gmail.com>
2024-09-21 10:45:48 +0530

    Delete Project directory

--- Project/AOOP_PROJECT1/src/facade/OrderProcessor.java
+++
@@ -1,8 +0,0 @@
-package facade;
-
-class OrderProcessor {
-    public void processOrder(String item) {
-        // Process the order
-        System.out.println("Order processed for " + item);
-    }
-}
